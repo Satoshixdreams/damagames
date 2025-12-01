02668ab658fefdc968909e851ea3a275c4079398
Kassim Hussein <kasoberlin@satoshi.fritz.box>
2025-12-01 11:56:25 +0100

    Remove v2.1 indicator and update build

--- App.tsx
+++ App.tsx
@@ -591,7 +591,7 @@ const App: React.FC = () => {
           <div className="flex-1 min-w-0">
             <div className="flex justify-between items-baseline mb-1">
               <h2 className="font-bold text-white text-base truncate pl-2">
-                {userProfile.username ? `@${userProfile.username}` : userProfile.rankTitle} <span className="text-[10px] text-slate-500 ml-1">(v2.1)</span>
+                {userProfile.username ? `@${userProfile.username}` : userProfile.rankTitle}
               </h2>
               <span className="text-xs text-slate-400 font-mono shrink-0">LVL {userProfile.level}</span>
             </div>
